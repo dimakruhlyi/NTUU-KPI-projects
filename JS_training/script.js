0199a7db34c4d03469597cd8720d04ea1686c448
Dima Kruhlyi <32203745+Dima2745@users.noreply.github.com>
2018-05-22 14:58:42 +0300

    Update script.js

--- JS_training/script.js
+++ JS_training/script.js
@@ -12,3 +12,18 @@ document.write("<b>"+admin+"</b>");
 var nameEarth = "Земля";
 var visitorName = "Dima";
 */
+
+/*
+var a = 1, b = 1, c, d;
+c = ++a; alert(c); // 2
+d = b++; alert(d); // 1
+c = (2+ ++a); alert(c); // 5
+d = (2+ b++); alert(d); // 4
+alert(a); // 3
+alert(b); // 3
+*/
+
+/*
+var a = 2;
+var x = 1 + (a *= 2);
+*/
