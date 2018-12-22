402199401ca191313a4c7b2b3037c46fe52312cf
Dima Kruhlyi <dimakruhly@gmail.com>
2018-12-22 18:56:26 +0200

    Add files via upload

---
+++ BIS/laba7/WindowsFormsApp1/Book_code.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Book_code : str
+    {
+        string[] st_key;
+        int count_line;
+        char[,] mas_key;
+
+        List<mas_count_letters> list = new List<mas_count_letters>();
+
+        public Book_code()
+        {
+            Book_code_Form new_form = new Book_code_Form();
+            new_form.Get_button4_Visible(false);
+            new_form.ShowDialog();
+
+            count_line = new_form.Get_count();
+            st_key = new string[count_line];
+            st_key = new_form.Get_Key();
+        }
+
+        public string encryption(string b)
+        {
+            String a = "";
+            int q = 0;
+            Random myRandom = new Random();
+
+            b = b.ToLower();
+            initial_mas();
+
+            while (q < b.Length)
+            {
+                a += find_index_letter_for_encryption(b[q], myRandom);
+                q++;
+            }
+            
+            return a;
+        }
+
+        public string decryption(string b)
+        {
+            String a = "";
+            int q = 0;
+
+            string[] result = b.Split(new char[] { '/', ' ' });
+            initial_mas();
+
+            while (q < result.Length - 1)
+            {
+                a += mas_key[Convert.ToInt32(result[q]), Convert.ToInt32(result[q + 1])];
+                q += 2;
+            }
+
+            return a;
+        }
+
+
+        private void initial_mas()
+        {
+            mas_key = new char[count_line, 10];
+
+            for (int i = 0; i < count_line; i++)
+            {
+                int count = 0;
+
+                try
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        st_key[i] = st_key[i].ToLower();
+                        while (st_letters.IndexOf(st_key[i][count]) == -1)
+                        {
+                            count++;
+                        }
+
+                        mas_key[i, j] = st_key[i][count];
+
+                        int result = IndexOf(list, st_key[i][count]);
+                        if (result != -1)
+                        {
+                            list[result].Count += 1;
+                        }
+                        else
+                        {
+                            list.Add(new mas_count_letters() { Letter = st_key[i][count], Count = 1 });
+                        }
+
+                        count++;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Кількість літер у рядку повинна бути більше 10", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+            }
+        }
+
+        private int IndexOf(List<mas_count_letters> list, char example)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Letter == example)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private string find_index_letter_for_encryption(char symbol, Random myRandom)
+        {
+            int index_list = IndexOf(list, symbol);
+            int value_random = -1;
+
+            if (index_list != -1)
+            {
+                value_random = myRandom.Next(1, list[index_list].Count + 1);
+            }
+            else
+            {
+                return "";
+            }
+
+            int count = 0;
+            for (int i = 0; i < count_line; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (symbol == mas_key[i, j])
+                    {
+                        count++;
+
+                        if (value_random == count)
+                        {
+                            return i.ToString() + "/" + j.ToString() + " ";
+                        }
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
---
+++ BIS/laba7/WindowsFormsApp1/Book_code_Form.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Book_code_Form : Form
+    {
+        public Book_code_Form()
+        {
+            InitializeComponent();
+            label3.Text = "Введіть N, S і \nпослідовність чисел у форматі X X X ... X";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count_line = richTextBox1.Lines.Length;
+            key = new string[count_line];
+            int count = count_line;
+            key_for_backpack = richTextBox1.Text;
+
+            if (richTextBox1.Text.Length != 0)
+            {
+                int number = 0;
+                for (int i = 0; i < count; i++)
+                {
+                    string q = richTextBox1.Lines[i];
+                    if (q != "")
+                    {
+                        key[number] = richTextBox1.Lines[i];
+                        number++;
+                    }
+                    else
+                    {
+                        count_line--;
+                    }
+                }
+            }
+
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oppenFile = new OpenFileDialog();
+
+            if (oppenFile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(oppenFile.FileName);
+            }
+        }
+
+        public void Get_button4_Visible(bool check_type)
+        {
+            if (check_type == true)
+            {
+                button4.Visible = true;
+                button1.Visible = false;
+
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+
+                label1.Visible = true;
+                label2.Visible = true;
+                label3.Visible = true;
+                label4.Visible = true;
+
+                richTextBox1.Size = new Size(richTextBox1.Size.Width, richTextBox1.Size.Height - 28);
+                richTextBox1.Location = new Point(richTextBox1.Location.X, 40);
+            }
+            else
+            {
+                button1.Visible = true;
+                button4.Visible = false;
+
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+
+                richTextBox1.Size = new Size(richTextBox1.Size.Width, 371);
+                richTextBox1.Location = new Point(richTextBox1.Location.X, 12);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Regex myRegex = new Regex(@"[0-9]+$", RegexOptions.IgnorePatternWhitespace);
+
+            int i = 0;
+
+            if (textBox1.Text != string.Empty && textBox2.Text != string.Empty && textBox3.Text != string.Empty && richTextBox1.Text != string.Empty)
+            {
+                while (richTextBox1.Text[richTextBox1.TextLength - i - 1] == ' ')
+                {
+                    i++;
+                }
+                richTextBox1.Text = richTextBox1.Text.Remove(richTextBox1.TextLength - i, i);
+
+                if (myRegex.IsMatch(richTextBox1.Text))
+                {
+                    key_for_backpack = richTextBox1.Text;
+                    N = Convert.ToInt32(textBox1.Text);
+                    M = Convert.ToInt64(textBox2.Text);
+                    T = Convert.ToInt64(textBox3.Text);
+
+                    if (NSD(M, T) == 1)
+                    {
+                        this.Close();
+                    }
+                }
+            }
+
+           
+        }
+
+
+        private long NSD(long a, long b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    long tmp = a;
+                    a = b;
+                    b = tmp;
+                }
+                b = b - a;
+            }
+            return a;
+        }
+
+        private void Book_code_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
