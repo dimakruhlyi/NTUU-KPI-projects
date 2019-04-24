97bd72697dbd76529e047d8435d6e9254ea4fe68
Dima Kruhlyi <dimakruhly@gmail.com>
2019-04-25 00:27:35 +0300

    Add files via upload

---
+++ NET Framework/Laba1/Laba1.2/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Reflection;
+
+//[assembly: AssemblyVersion("2.0.0.0")]
+namespace Laba1._2
+{
+    public class Circle
+    {
+        int radius;
+        int a, b;
+        public double P()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public Circle(int a2, int b2, int radius2)
+        {
+            a = a2;
+            b = b2;
+            radius = radius2;
+        }
+        public Circle()
+        {
+            a = 0;
+            b = 0;
+            radius = 1;
+        }
+    }
+}
