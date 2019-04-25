4989d5cbc342ac892d4c6a5123cce3eea9cb5373
Dima Kruhlyi <dimakruhly@gmail.com>
2019-04-26 01:12:15 +0300

    Add files via upload

---
+++ NET Framework/Laba2/LinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        public Node<T> next;
+        public T element;
+
+        public Node(T value)
+        {
+            element = value;
+            next = null;
+        }
+
+        public Node()
+        {
+            next = null;
+        }
+    }
+}
