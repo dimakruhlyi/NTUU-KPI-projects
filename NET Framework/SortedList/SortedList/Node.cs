fa118bdaac5186a6068dbf7441e21cecb0dc3196
Dima Kruhlyi <dimakruhly@gmail.com>
2019-06-10 09:29:21 +0300

    Add files via upload

---
+++ NET Framework/SortedList/SortedList/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortedList;
+
+namespace SortedList
+{
+    internal class Node<T>
+    {
+        internal T Value { get; }
+        internal Node<T> Next;
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+
+        public Node()
+        {
+            Next = null;
+        }
+    }
+        
+}
