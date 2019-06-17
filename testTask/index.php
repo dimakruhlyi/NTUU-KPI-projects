1d17bb67adaea1a350b58c246067a352c8f6017b
Dima Kruhlyi <dimakruhly@gmail.com>
2019-06-17 21:59:13 +0300

    Update index.php
    Added task 2 (check meetings time function)

--- testTask/index.php
+++ testTask/index.php
@@ -20,4 +20,28 @@
         echo "$values,";
     }
 
+ //                                    TASK 2 (Check meetings time function) 
+$meetings = "1630 2030";
+function check_meetings($str){
+    $value_1 = (int)substr($str,0,2);
+    $value_2 = (int)substr($str,5,2);
+    $value_1_1 = (int)substr($str,2,2);
+    $value_2_2 = (int)substr($str,7,2);
+    
+    
+    if($value_1>$value_2){
+        echo " ERROR!! <br/> $value_1 hours more than $value_2 hours!";
+    }
+    else{
+        if(($value_1 == $value_2) and ($value_1_1>$value_2_2)){
+            echo " ERROR!! <br/> $value_1 hours $value_1_1 minutes less than $value_2 hours $value_2_2 minutes!";
+        }
+        else{
+            echo " Time of metting is correct!";
+        }
+    }
+}
+
+check_meetings($meetings);
+
 ?>
