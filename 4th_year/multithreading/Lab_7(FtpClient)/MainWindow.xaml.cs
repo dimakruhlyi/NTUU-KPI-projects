a39aa878a8b7dbf01e7cda37d351307afc4fafad
Dima Kruhlyi <dimakruhly@gmail.com>
2019-12-23 21:15:07 +0200

    Add files via upload

---
+++ 4th_year/multithreading/Lab_7(FtpClient)/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FtpClient
+{
+    public partial class MainWindow : Window
+    {
+        string prevAdress = "ftp://";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_connect_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Создаем объект подключения по FTP
+                Client client = new Client(txt_adres.Text, txt_login.Text, txt_password.Password);
+
+                // Регулярное выражение, которое ищет информацию о папках и файлах 
+                // в строке ответа от сервера
+                Regex regex = new Regex(@"^([d-])([rwxt-]{3}){3}\s+\d{1,}\s+.*?(\d{1,})\s+(\w+\s+\d{1,2}\s+(?:\d{4})?)(\d{1,2}:\d{2})?\s+(.+?)\s?$",
+                    RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+                // Получаем список корневых файлов и папок
+                // Используется LINQ to Objects и регулярные выражения
+                List<FileDirectoryInfo> list = client.ListDirectoryDetails()
+                                                     .Select(s =>
+                                                     {
+                                                         Match match = regex.Match(s);
+                                                         if (match.Length > 5)
+                                                         {
+                                                             // Устанавливаем тип, чтобы отличить файл от папки (используется также для установки рисунка)
+                                                             string type = match.Groups[1].Value == "d" ? "DIR.png" : "FILE.png";
+
+                                                             // Размер задаем только для файлов, т.к. для папок возвращается
+                                                             // размер ярлыка 4кб, а не самой папки
+                                                             string size = "";
+                                                             if (type == "FILE.png")
+                                                                 size = (Int32.Parse(match.Groups[3].Value.Trim()) / 1024).ToString() + " кБ";
+
+                                                             return new FileDirectoryInfo(size, type, match.Groups[6].Value, match.Groups[4].Value, txt_adres.Text);
+                                                         }
+                                                         else return new FileDirectoryInfo();
+                                                     }).ToList();
+
+                // Добавить поле, которое будет возвращать пользователя на директорию выше
+                list.Add(new FileDirectoryInfo("","DEFAULT.png","...","",txt_adres.Text));
+                list.Reverse();
+
+                // Отобразить список в ListView
+                lbx_files.DataContext = list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+            }
+        }
+
+        private void folder_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount >= 2)
+            {
+                FileDirectoryInfo fdi = (FileDirectoryInfo)(sender as StackPanel).DataContext;
+                if (fdi.Type == "DIR.png")
+                {
+                    prevAdress = fdi.adress;
+                    txt_adres.Text = fdi.adress + fdi.Name + "/";
+                    btn_connect_Click_1(null, null);
+                }
+            }
+        }
+    }
+}
---
+++ 4th_year/multithreading/Lab_7(FtpClient)/Resources/WindowTemplate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace FtpClient
+{
+    public partial class WindowTemplate : ResourceDictionary
+    {
+        private bool flipWindow = false;
+        double workHeight = SystemParameters.WorkArea.Height;
+        double workWidth = SystemParameters.WorkArea.Width;
+
+        public WindowTemplate()
+        {
+            InitializeComponent();
+        }
+
+        private void TopTitle_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://professorweb.ru");
+        }
+
+        #region "Сворачивание, разворачивание, закрытие, перетаскивание окна"
+        private void closeWindow(object sender, RoutedEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            win.Close();
+        }
+
+        private void minimizeWindow(object sender, RoutedEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            win.WindowState = WindowState.Minimized;
+        }
+
+        private void maximizedWindow(object sender, RoutedEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            Button btn = (Button)sender;
+
+            if (!flipWindow)
+            {
+                flipWindow = true;
+                win.Height = workHeight * 0.75;
+                win.Width = workWidth * 0.75;
+                win.Top = workHeight / 8;
+                win.Left = workWidth / 8;
+                btn.Content = Application.Current.FindResource("DataButtonMaximize");
+            }
+            else
+            {
+                flipWindow = false;
+                win.Height = workHeight;
+                win.Width = workWidth;
+                win.Top = 0;
+                win.Left = 0;
+                btn.Content = Application.Current.FindResource("DataButtonMinimize");
+            }
+        }
+
+        private void title_MouseMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Window win = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (flipWindow)
+                win.DragMove();
+        }
+
+        #endregion
+
+        private void timeline_MouseMove(object sender, MouseEventArgs e)
+        {
+            FrameworkElement element = (FrameworkElement)sender;
+            double x = e.GetPosition(element).X;
+            ToolTip tt = (ToolTip)element.ToolTip;
+            tt.HorizontalOffset = x - tt.ActualWidth / 2;
+        }
+
+    }
+}
