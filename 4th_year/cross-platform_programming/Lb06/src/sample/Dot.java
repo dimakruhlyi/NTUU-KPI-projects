92038879a50869a86f8e26b656cb3706981c9d88
Dima Kruhlyi <dimakruhly@gmail.com>
2020-04-06 20:42:32 +0300

    Add files via upload

---
+++ 4th_year/cross-platform_programming/Lb06/src/sample/Dot.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class Dot {
+    public double x;
+    public double y;
+
+    public Dot () {
+
+    }
+
+    public Dot (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
